4bbffe6281cf27725a76dbdbcf3ef14aa86429bb
gparedes10 <german10paredes@gmail.com>
2022-04-03 20:13:48 -0400

    Added Leaflet Map
    -Added necessary functions for Leaflet Map
    -Fixed bugs in Reactive Data function

--- App.R
+++ App.R
@@ -25,7 +25,15 @@ neighborhoods <- st_read("Neighborhoods.geojson") #Read Neighborhood File
 #----------------------------------------------
 #Map Labels for Neighborhood Names
 #----------------------------------------------
+labels <- sprintf(
+    "<strong>%s</strong>",
+    neighborhoods$name
+) %>% lapply(htmltools::HTML)
 
+#make color pallet for service requests
+#use hue_pal()(10) to add the 10 base colors from ggplot to see the 10 colors use show_col(hue_pal()(10))
+#Use colorFactor(rainbow(10)) to test other colors
+factpal = colorFactor(hue_pal()(14), crime_Data$Description)
 
 
 ################################################################################
@@ -75,7 +83,8 @@ ui <- fluidPage(
             #----------------------------------------------
             #Map
             #----------------------------------------------
-            
+            h1("Crime Map"),
+            leafletOutput("crime_map", width = "90%", height = "600px")
             
             
             
@@ -115,9 +124,9 @@ server <- function(input, output, session) {
     data <- reactive({
         req(input$crime_choice, input$neighborhood_choice)
         
-        filtered_data <- sr_311Data %>%
-            filter(srtype %in% input$crime_choice) %>%
-            filter(neighborhood %in% input$neighborhood_choice) #%>%
+        filtered_data <- crime_Data %>%
+            filter(Description %in% input$crime_choice) %>%
+            filter(Neighborhood %in% input$neighborhood_choice) #%>%
             #filter(createddate >= input$daterange[1] & createddate <= input$daterange[2])
         
     })
@@ -138,9 +147,52 @@ server <- function(input, output, session) {
     #----------------------------------------------
     #Leaflet Map
     #----------------------------------------------
+    output$crime_map<- renderLeaflet({
+        leaflet() %>%
+            addTiles() %>% #adds the base map
+            
+            addPolygons( #adds the polygon layer for the neighborhoods
+                data = neighborhoods,
+                color = "#444444", #polygon color
+                weight = 1.5,      #polygons' outline weight
+                opacity = 1,       #polygons' outline opacity
+                fillOpacity = 0.25, #polygon opacity
+                highlightOptions = highlightOptions(
+                    color = "white",  #Highlights the neighborhood hovered
+                    weight = 3, 
+                    bringToFront = FALSE), #moves the neighborhood outline layer up
+                label = labels, #adds neighborhood name label when hovered
+                labelOptions = labelOptions(
+                    textsize = "14px"),
+                layerId = ~name, #Used to get the name of the neighborhood when clicking on the map
+                group = "neighborhoodShapes") %>%
     
+            addCircleMarkers( #adds layer with Service Requests markers
+                data = data(),
+                color = ~factpal(Description), #change color for each type of request based on color pallet
+                stroke = FALSE,           #remove the outer line of the marker
+                fillOpacity = 0.75,       
+                popup = data()$Description,    #adds popup when marker is clicked
+                group = "crimes") %>%  
+            
+            addLayersControl(
+                overlayGroups = c("crimes","neighborhoodShapes"),
+                options = layersControlOptions(collapsed = FALSE)) %>%
+            
+            addLegend(pal = factpal, values = data()$Description, opacity = 1)
+    })
     
-    
+    #Map zooms into the neighborhood that the user clicks on
+    #leafletProxy makes it so that the map doesn't have to be redrawn whenever the user clicks on it.
+    observeEvent(input$crime_map_shape_click, {
+        click <- input$crime_map_shape_click
+        
+        leafletProxy("crime_map") %>%
+            setView(
+                lng = click$lng,
+                lat = click$lat,
+                zoom = 15)
+    })
     
     #----------------------------------------------
     #Total Crimes Per Neighborhood Graph
